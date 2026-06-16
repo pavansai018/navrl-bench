7c6ef3aa45d2589f80556b9f18583e09deef3fe6
pavansai018 <18pavansai@gmail.com>
2026-06-16 20:50:14 +0800

    ppo cfg file for local planner task

---
+++ training/dynamic_obstacle_avoidance/source/dynamic_obstacle_avoidance/dynamic_obstacle_avoidance/tasks/manager_based/local_planner/agents/rsl_rl_ppo_cfg.py
@@ -0,0 +1,39 @@
+
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from isaaclab.utils import configclass
+
+from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, RslRlPpoAlgorithmCfg
+
+
+@configclass
+class PPORunnerCfg(RslRlOnPolicyRunnerCfg):
+    num_steps_per_env = 32
+    max_iterations = 8000
+    save_interval = 200
+    experiment_name = "m3_local_planner"
+    policy = RslRlPpoActorCriticCfg(
+        init_noise_std=0.3, #1.0,
+        actor_obs_normalization=True, #False,
+        critic_obs_normalization=True, #False,
+        actor_hidden_dims=[256, 256, 128],
+        critic_hidden_dims=[256, 256, 128],
+        activation="elu",
+    )
+    algorithm = RslRlPpoAlgorithmCfg(
+        value_loss_coef=1.0,
+        use_clipped_value_loss=True,
+        clip_param=0.2,
+        entropy_coef=0.0, #0.005,
+        num_learning_epochs=5,
+        num_mini_batches=4,
+        learning_rate=5.0e-4, #1.0e-3, # 5.0e-4
+        schedule="adaptive",
+        gamma=0.99,
+        lam=0.95,
+        desired_kl=0.01,
+        max_grad_norm=1.0,
+    )
